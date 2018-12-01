e2f577d6c2a8d9276ae2715d15e4199a79e13d3a
rnrpalacios <rnrpalacios@gmail.com>
2018-12-01 00:16:43 -0300

    first commit

---
+++ src/main/java/com/xpen6/xpen6/Xpen6Application.java
@@ -0,0 +1,12 @@
+package com.xpen6.xpen6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xpen6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Xpen6Application.class, args);
+	}
+}
---
+++ src/test/java/com/xpen6/xpen6/Xpen6ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xpen6.xpen6;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Xpen6ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
